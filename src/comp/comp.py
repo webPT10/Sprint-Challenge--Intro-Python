7b0b161513ecff72cf4164cea54d1d1be93ae5fa
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-08-14 16:56:25 -0700

    comp > q1, getting error TypeError: 'str' object is not callable

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,7 +24,7 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [h.name for h in humans if h.name('D')]
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
