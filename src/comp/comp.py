3a58bc6d3cef172b5f3a140e66efd507a139d6f3
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-08-14 17:14:21 -0700

    comp > q3, skipped >> q4, completed

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -41,7 +41,7 @@ print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [h.age + 10 for h in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
